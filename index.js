a557738a854d341ba0dc7a9ae1168374465a9d82
huynvq <quanghuy.vnu65@gmail.com>
2024-03-04 14:59:08 +0700

    update

--- index.js
+++ index.js
@@ -6,25 +6,43 @@ const { serverUrl } = JSON.parse(fs.readFileSync(configPath, "utf8"));
 const wrap = (s) => "{ return " + s + " };";
 
 (async () => {
-  const res = await fetch(`${serverUrl}/api/default-process`);
-  let allParams = {};
-  const process = await res.json();
-  for (const interaction of process.interactions) {
+  const waitFor = async (time) => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve();
+      }, time);
+    });
+  };
+  const execute = async () => {
+    const res = await fetch(`${serverUrl}/api/default-process`);
+    let allParams = {};
+    const process = await res.json();
+    for (const interaction of process.interactions) {
+      try {
+        console.log(":============>interaction", interaction);
+        const executeInit = new Function(wrap(interaction.content));
+        const params = JSON.parse(interaction.params);
+        console.log(":============>execute function", interaction);
+        if (params) {
+          allParams = { ...allParams, ...params };
+        }
+        let result = await executeInit.call(null).call(null, allParams);
+        if (result) {
+          allParams = { ...allParams, ...result };
+        }
+        console.log(allParams);
+      } catch (error) {
+        console.log(":============>error", error.message);
+      }
+    }
+  };
+
+  for (let retryCount = 0; retryCount < 100000; retryCount++) {
     try {
-      console.log(":============>interaction", interaction);
-      const executeInit = new Function(wrap(interaction.content));
-      const params = JSON.parse(interaction.params);
-      console.log(":============>execute function", interaction);
-      if (params) {
-        allParams = { ...allParams, ...params };
-      }
-      let result = await executeInit.call(null).call(null, allParams);
-      if (result) {
-        allParams = { ...allParams, ...result };
-      }
-      console.log(allParams);
+      await execute();
     } catch (error) {
-      console.log(":============>error", error.message);
+      consosle.log("error", error.message);
+      await waitFor(60000);
     }
   }
 })();
